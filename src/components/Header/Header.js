83ab6a2cc75d4d560e47665acf9d459330c3df56
Ashish Singh <6ashishsingh@gmail.com>
2023-03-29 20:29:15 +0530

    show hide menu icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.scss';
 const Header = (props) => {
+
+    const [showMenu, setShowMenu] = useState(true);
+
+    const handleMenuClick = () => {
+        setShowMenu(!showMenu);
+    }
 
     return (
         <div className="header">
             <div className="container">
                 <a className="brand">Ashish Singh</a>
-                <div className="nav-dropdown">
-                    
+                <div className="menu">
+                    <i className={`menu-icon fa fa-times ${showMenu ? 'show-element' : 'hide-element'}`} onClick={handleMenuClick}></i>
+                    <i className={`menu-icon fa fa-bars ${!showMenu ? 'show-element' : 'hide-element'}`} onClick={handleMenuClick}></i>
                 </div>
             </div>
         </div>
