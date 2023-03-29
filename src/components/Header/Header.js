54706f33531eb34d18eaee6851c59dec2f415eae
Ashish Singh <6ashishsingh@gmail.com>
2023-03-29 20:10:29 +0530

    Resume website first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Header.scss';
+const Header = (props) => {
+
+    return (
+        <div className="header">
+            <div className="container">
+                <a className="brand">Ashish Singh</a>
+                <div className="nav-dropdown">
+                    
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Header;
