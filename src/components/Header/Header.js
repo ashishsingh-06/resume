4a198b08c4a640bac2fde7705baf9d622fb7b267
Ashish Singh <6ashishsingh@gmail.com>
2023-03-30 01:50:39 +0530

    Added Menu Items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import './Header.scss';
+
+const itemVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: {type: "spring", stiffness: 300, damping: 24 }
+    },
+    closed: { opacity: 0, y: -20, transition: { duration: 0.2 } }
+  };
+
 const Header = (props) => {
 
     const [showMenu, setShowMenu] = useState(true);
@@ -11,10 +22,43 @@ const Header = (props) => {
     return (
         <div className="header">
             <div className="container">
-                <a className="brand">Ashish Singh</a>
+                {/* change brand to AS insde a div like a logo */}
+                <a className="brand">Ashish Singh</a> 
                 <div className="menu">
                     <i className={`menu-icon fa fa-times ${showMenu ? 'show-element' : 'hide-element'}`} onClick={handleMenuClick}></i>
                     <i className={`menu-icon fa fa-bars ${!showMenu ? 'show-element' : 'hide-element'}`} onClick={handleMenuClick}></i>
+                    <motion.nav
+                        // variants={{
+                        //     open: {
+                        //       clipPath: "circle(0% 0% 0% 0% round 0px)",
+                        //       transition: {
+                        //         type: "spring",
+                        //         bounce: 0,
+                        //         duration: 0.7,
+                        //         delayChildren: 0.3,
+                        //         staggerChildren: 0.05
+                        //       }
+                        //     },
+                        //     closed: {
+                        //       clipPath: "circle(10% 50% 90% 50% round 10px)",
+                        //       transition: {
+                        //         type: "spring",
+                        //         bounce: 0,
+                        //         duration: 0.3
+                        //       }
+                        //     }
+                        //   }}
+                        variants={itemVariants}
+                        animate={showMenu ? "open" : "closed"} 
+                        className={`nav`}>
+                        <motion.ul
+                            className="nav-list">
+                            <motion.li variants={itemVariants} className="nav-list-item"><a>Home</a></motion.li>
+                            <motion.li variants={itemVariants} className="nav-list-item"><a>Portfolio</a></motion.li>
+                            <motion.li variants={itemVariants} className="nav-list-item"><a>Experience</a></motion.li>
+                            <motion.li variants={itemVariants} className="nav-list-item"><a>About</a></motion.li>
+                        </motion.ul>
+                    </motion.nav>
                 </div>
             </div>
         </div>
